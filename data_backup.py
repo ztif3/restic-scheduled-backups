a41301e25a27b6be44d356f02232f38476614158
Ryan Fitz-Gerald <ztif33@gmail.com>
2025-04-29 12:41:17 -0400

    set pw_file and paths to positional parameters added dry run parameter

--- data_backup.py
+++ data_backup.py
@@ -11,14 +11,15 @@ def main():
     parser = argparse.ArgumentParser(description='Run data backup script.')
     
     parser.add_argument('repo', type=str, help='repo path for the backup')
-    parser.add_argument('--pw_file', type=str, help='Password file for the restic repo')
-    parser.add_argument('--paths', type=str, nargs='+', help='Paths to backup')
+    parser.add_argument('pw_file', type=str, help='Password file for the restic repo')
+    parser.add_argument('paths', type=str, nargs='+', help='Paths to backup')
     parser.add_argument('--include_files', type=str, nargs='*', help='List of files with paths to include in the backup')
     parser.add_argument('--exclude_files',type=str, nargs='*', help='List of files with paths to exclude in the backup')
     parser.add_argument('--ret_days', type=int, help='Number of days to keep the backup', default=14)
     parser.add_argument('--ret_weeks', type=int, help='Number of weeks to keep the backup', default=16)
     parser.add_argument('--ret_months', type=int, help='Number of months to keep the backup', default=18)
     parser.add_argument('--ret_years', type=int, help='Number of years to keep the backup', default=3)
+    parser.add_argument('--dry_run', action='store_true', help='Run the script in dry run mode')
 
     args = parser.parse_args()
 
@@ -26,12 +27,13 @@ def main():
 
     
 
-def init_repo(repo: Path | str,pw_file: Path | str):
+def init_repo(repo: Path | str,pw_file: Path | str, dry_run: bool = False):
     """ Initialize the repo if it does not already exist.
 
     Args:
         repo (Path | str): path to the restic repo
         pw_file (Path | str): password file for the restic repo
+        dry_run (bool): if True, run the script in dry run mode
     """
     
     # Set repo parameters
@@ -46,8 +48,9 @@ def data_backup(
         repo: Path | str, 
         pw_file: Path | str, 
         paths: list[Path|str],
-        include_files: list[Path|str], 
-        exclude_files: list[Path|str], 
+        include_files: list[Path|str]=[], 
+        exclude_files: list[Path|str]=[], 
+        dry_run: bool = False
     ):
     """ Function to run the data backup script.
 
@@ -57,6 +60,7 @@ def data_backup(
         paths (list[Path | str]): paths to backup
         include_files (list[Path | str]): List of files with paths to include in the backup
         exclude_files (list[Path | str]): List of files with paths to exclude in the backup
+        dry_run (bool): if True, run the script in dry run mode
     """
     # Set repo parameters
     restic.repo = repo
@@ -65,12 +69,18 @@ def data_backup(
     # TODO Check if repo exists
 
     # Run Restic Backup
-    result = restic.backup(paths=paths,include_files=include_files, exclude_files=exclude_files, skip_if_unchanged=True)
+    result = restic.backup(
+        paths=paths,
+        include_files=include_files, 
+        exclude_files=exclude_files, 
+        skip_if_unchanged=True, 
+        dry_run=dry_run
+    )
 
     # TODO Log results of the backup
     # TODO Send notifications of backup results
 
-def clean_repo(repo: Path | str,pw_file: Path | str, ret_days: int, ret_weeks: int, ret_months: int, ret_years: int):
+def clean_repo(repo: Path | str,pw_file: Path | str, ret_days: int, ret_weeks: int, ret_months: int, ret_years: int, dry_run: bool = False):
     """ Clean the repo by removing old snapshots.
 
     Args:
@@ -80,6 +90,7 @@ def clean_repo(repo: Path | str,pw_file: Path | str, ret_days: int, ret_weeks: i
         ret_weeks (int): Number of weeks to keep the backup
         ret_months (int): Number of months to keep the backup
         ret_years (int): Number of years to keep the backup
+        dry_run (bool): if True, run the script in dry run mode
     """
     # Set repo parameters
     restic.repo = repo
@@ -91,7 +102,8 @@ def clean_repo(repo: Path | str,pw_file: Path | str, ret_days: int, ret_weeks: i
         keep_daily=ret_days,
         keep_weekly=ret_weeks,
         keep_monthly=ret_months,
-        keep_yearly=ret_years
+        keep_yearly=ret_years,
+        dry_run=dry_run
     )
 
     # TODO Log results of the cleanup

7d07c5bc23484e4057777a9c48a85d26b768899a
Ryan Fitz-Gerald <ztif33@gmail.com>
2025-05-23 15:08:02 -0400

    Cleaned up mounted drive search

--- system.py
+++ system.py
@@ -1,66 +1,63 @@
 import json
 import logging
+from os import PathLike
 import subprocess
 from pathlib import Path
+from typing import Optional, TypedDict
 
 logger = logging.getLogger(__name__)
 
-def list_mounted_partitions() -> dict[str:list[str]]:
-    """ Generates a list of mounted partitions with a list of their mountpoints
+def list_mounted_partitions() -> dict[str,list[PathLike]]:
+        """ Generates a list of mounted partitions with a list of their mountpoints
 
-    Returns:
-        dict[str:list[str]]: dictionary with the names of the mounted partitions as keys and a list
-                             of all mount points as the values
-    """
+        Returns:
+            dict[str, list[PathLike]]: 
+        """
 
-    mount_list:dict[str:list[str]] = {}
-    logger.info('Listing all mounted partitions')
-    try:
-        output = subprocess.run(['lsblk','-J','-o','+LABEL'], capture_output=True)
-    except subprocess.CalledProcessError as e:
-        logger.error('Unable to get list of all mounted partitions')
-        raise
-    finally:
-        output_dict = json.loads(output)
+        mount_list:dict[str, list[PathLike]] = {}
+        output = ""
+        logger.info('Listing all mounted partitions')
+        try:
+            output:str = str(subprocess.run(['lsblk','-J','-o','+LABEL'], capture_output=True))
+        except subprocess.CalledProcessError as e:
+            logger.error('Unable to get list of all mounted partitions')
+            raise
+        finally:
+            output_dict = json.loads(output)
 
-        # Check if block devices exist
-        if 'blockdevices' in output_dict:
-            # Loop through all block devices
-            for device in output_dict['blockdevices']:
-                # Get device name
-                device_name = 'unnamed device'
-                
-                if 'name' in device:
-                    device_name = device['name']
-
-                # Check if the device has any children
-                if 'children' in device:
-                    for child in device['children']:
-                        # Get child name
-                        child_name = 'unnamed child' 
-                        
-                        if 'name' in child:
-                            child_name = child['name']
-
-                        # Check if child has mountpoints
-                        if 'mountpoints' in child:
-                            mount_list[child_name] = child['mountpoints']
-
-                            logger.debug(f'{len(mount_list[child_name])} mount points found for child "{child_name}" for block device "{device_name}"')
-
-                        else:
-                            logger.debug(f'No Mount points found in child "{child_name}" for block device "{device_name}"')
-
-
-                else:
+            # Check if block devices exist
+            if 'blockdevices' in output_dict:
+                # Loop through all block devices
+                for device in output_dict['blockdevices']:
+                    # Get device name
+                    device_name = 'unnamed device'
                     
-                    logger.debug(f'No children found in block device "{device_name}"')
-        else:
-            logger.error('no block devices found in lsblk output')
+                    if 'name' in device:
+                        device_name = device['name']
 
-    return mount_list
+                    # Check if the device has any children
+                    if 'children' in device:
+                        for child in device['children']:
+                            # Get child name
+                            child_name = 'unnamed child' 
+                            
+                            if 'name' in child:
+                                child_name = child['name']
+
+                            # Check if child has mountpoints
+                            if 'mountpoints' in child:
+                                mount_list[child_name] = [Path(mp) for mp in child['mountpoints']]
+
+                                logger.debug(f'{len(mount_list[child_name])} mount points found for child "{child_name}" for block device "{device_name}"')
+
+                            else:
+                                logger.debug(f'No Mount points found in child "{child_name}" for block device "{device_name}"')
 
 
-    
+                    else:
+                        
+                        logger.debug(f'No children found in block device "{device_name}"')
+            else:
+                logger.error('no block devices found in lsblk output')
 
-
+        return mount_list

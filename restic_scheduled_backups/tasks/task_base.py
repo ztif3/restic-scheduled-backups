94cb88178a9f9cf2ab37709f0a38cbd3bf6209f8
Ryan Fitz-Gerald <ztif33@gmail.com>
2025-07-14 09:58:15 -0400

    Moved task skipping to queueing

--- restic_scheduled_backups/tasks/task_base.py
+++ restic_scheduled_backups/tasks/task_base.py
@@ -12,16 +12,17 @@ from restic_scheduled_backups.util.system import *
 
 import restic_scheduled_backups.common
 
-task_queue = Queue() 
+task_queue = Queue()
 
 logger = logging.getLogger(__name__)
+
 
 class TaskBase(ABC):
     def __init__(self,
                  name: str,
                  task_config: TaskConfig,
                  ntfy_config: Optional[NtfyConfig] = None,
-    ):
+                 ):
         """ Constructor
 
         Args:
@@ -42,15 +43,17 @@ class TaskBase(ABC):
 
         self.task_queued = Event()
 
-
     def schedule(self) -> None:
-        logger.info(f'Scheduling task: {self.name} - {self.update_period.type} - freq:{self.update_period.frequency} - at:{self.update_period.run_time}')
+        logger.info(
+            f'Scheduling task: {self.name} - {self.update_period.type} - freq:{self.update_period.frequency} - at:{self.update_period.run_time}')
         # Queue task
         match(self.update_period.type):
             case PeriodType.HOURLY:
-                schedule.every(self.update_period.frequency).hours.do(self.__queue_task)
+                schedule.every(self.update_period.frequency).hours.do(
+                    self.__queue_task)
             case PeriodType.DAILY:
-                schedule.every(self.update_period.frequency).days.at(self.update_period.run_time).do(self.__queue_task) 
+                schedule.every(self.update_period.frequency).days.at(
+                    self.update_period.run_time).do(self.__queue_task)
             case PeriodType.WEEKLY:
                 match(self.update_period.weekday):
                     case WeekdayType.SUNDAY:
@@ -66,42 +69,46 @@ class TaskBase(ABC):
                     case WeekdayType.FRIDAY:
                         schedule.every().friday.at(self.update_period.run_time).do(self.__queue_task)
                     case WeekdayType.SATURDAY:
-                        schedule.every().saturday.at(self.update_period.run_time).do(self.__queue_task) 
+                        schedule.every().saturday.at(self.update_period.run_time).do(self.__queue_task)
                     case _:
                         schedule.every().weeks.at(self.update_period.run_time).do(self.__queue_task)
-        
+
     def __queue_task(self):
         """ Schedule the task """
-        
+
         if not self.task_queued.is_set():
-            try:
-                self.task_queued.set()
-                task_queue.put(self)
-                logging.info(f'queued task: {self.name}')
-            except:
-                logging.error(f'Failed to queue task {self.name}')
-                self.task_queued.clear()
+            skipped = self.update_period.type != PeriodType.WEEKLY or self.update_period.weekday is None or self.skip_count > self.update_period.frequency - 1
+
+            if skipped:
+                self.skip_count = 0
+
+                try:
+                    self.task_queued.set()
+                    task_queue.put(self)
+                    logging.info(f'queued task: {self.name}')
+                except:
+                    logging.error(f'Failed to queue task {self.name}')
+                    self.task_queued.clear()
+            else:
+                self.skip_count += 1
+                logger.info(
+                    f'Skipping task: {self.name} due to frequency or weekday settings. Skipping count: {self.skip_count} of {self.update_period.frequency}')
+
         else:
-            logging.warning(f'Attempted to queue already scheduled task: {self.name}')
+            logging.warning(
+                f'Attempted to queue already scheduled task: {self.name}')
 
     def start_task(self):
         """ Handles running tasks """
 
-        if self.update_period.type != PeriodType.WEEKLY or self.update_period.weekday is None or self.skip_count > self.update_period.frequency - 1:
-            self.skip_count = 0
-            logger.info(f'Starting task: {self.name}')
-            self.run()
-            logger.info(f'Task Complete: {self.name}')
-
-        else:
-            logger.info(f'Skipping task: {self.name} due to frequency or weekday settings. Skipping count: {self.skip_count} of {self.update_period.frequency - 1}')
-            self.skip_count += 1
+        logger.info(f'Starting task: {self.name}')
+        self.run()
+        logger.info(f'Task Complete: {self.name}')
 
         self.task_queued.clear()
 
     @abstractmethod
     def run(self):
         """ Run the task """
-        logger.warning(f'Task {self.name} has no implementation for run() method. This should be overridden by subclasses.')
-
-        
+        logger.warning(
+            f'Task {self.name} has no implementation for run() method. This should be overridden by subclasses.')
